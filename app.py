153b1f2da8ba59968ed08bb5d6f6cc2f3912c1e1
BogdanAnghelina <bogdan_anghelina@Yahoo.com>
2023-08-19 21:57:26 +0000

    added more functionalities (tabs, variables)

--- app.py
+++ app.py
@@ -1,39 +1,27 @@
 from flask import Flask, render_template, redirect, url_for, flash, request
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
-from flask_sqlalchemy import SQLAlchemy
+from flask_login import current_user, LoginManager, UserMixin, login_user, login_required, logout_user
+from database import Template, session
 
 app = Flask(__name__)
-app.config[
-  'SECRET_KEY'] = 'your_secret_key_here'  # change this to a strong secret key
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///templates.db'
-db = SQLAlchemy(app)
-
-class Template(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
-    content = db.Column(db.Text, nullable=False)
+app.config['SECRET_KEY'] = 'your_secret_key_here'
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
 class User(UserMixin):
-    pass
+    def __init__(self, user_id):
+        self.id = user_id
 
 @login_manager.user_loader
 def load_user(user_id):
-    # For now, we aren't using a database, so we'll mock a user object
-    user = User()
-    user.id = user_id
-    return user
+    return User(user_id=user_id)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        # hardcoded username and password for demonstration
         if request.form['username'] == 'admin' and request.form['password'] == 'password':
-            user = User()
-            user.id = 'admin'
+            user = User(user_id='admin')
             login_user(user)
             return redirect(url_for('new_template'))
         else:
@@ -43,23 +31,54 @@ def login():
 @app.route('/new_template', methods=['GET', 'POST'])
 @login_required
 def new_template():
+    template = session.query(Template).filter_by(draft=True, user_id=current_user.id).first()
+
+    if not template:
+        draft_name = "Draft #1"
+        template = Template(template_name=draft_name, template_content='', draft=True, user_id=current_user.id)
+        session.add(template)
+        session.commit()
+
     if request.method == 'POST':
-        template_name = request.form['template_name']
-        template_content = request.form['template_content']
+        if 'save_template' in request.form:
+            template_name = request.form['template_name']
+            template_content = request.form['template_content']
 
-        new_template = Template(name=template_name, content=template_content)
-        db.session.add(new_template)
-        db.session.commit()
+            template.template_name = template_name or "Draft #1"
+            template.template_content = template_content or ''
+            session.commit()
 
-        flash('Template saved successfully!')
-        return redirect(url_for('new_template'))
-    return render_template('new_template.html')
+            flash('Template saved successfully!')
+            return redirect(url_for('new_template'))
+
+        elif 'add_variable' in request.form:
+            variable_name = request.form['variable_name']
+            if variable_name:
+                # Add the variable to the template's variables list
+                current_variables = template.template_variables.split(",") if template.template_variables else []
+                current_variables.append(variable_name)
+                template.template_variables = ",".join(current_variables)
+                session.commit()
+                flash('Variable added successfully!')
+                return redirect(url_for('new_template'))
+
+    variables = template.template_variables.split(",") if template.template_variables else []
+
+    return render_template('new_template.html', variables=variables)
 
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    if exception:
+        session.rollback()
+    else:
+        session.commit()
+    session.close()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,21 +1,30 @@
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, Column, Integer, String, Text, ForeignKey, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
 import os
 
 db_connection_string = os.environ['db_connection_string']
 
 engine = create_engine(
-  db_connection_string,
-  connect_args={
-    "ssl": {
-      "ssl_ca": "/etc/ssl/cert.pem"
+    db_connection_string,
+    connect_args={
+        "ssl": {
+            "ssl_ca": "/etc/ssl/cert.pem"
+        }
     }
-  })
+)
 
-with engine.connect() as conn:
-    result = conn.execute(text("select * from templates"))
-  
-    result_dicts = []
-    for row in result:
-        result_dicts.append(row._asdict())
+Session = sessionmaker(bind=engine)
+session = Session()
 
-    print(result_dicts)
+Base = declarative_base()
+
+class Template(Base):
+    __tablename__ = 'templates'
+    
+    id = Column(Integer, primary_key=True)
+    template_name = Column(String(200), nullable=False)
+    template_content = Column(Text, nullable=False)
+    template_variables = Column(Text, nullable=True)
+    draft = Column(Boolean, default=True)
+    user_id = Column(String(50), nullable=False)
