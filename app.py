cbd7932edc8a177b1fcf130779e8ec5a3e548a82
BogdanAnghelina <bogdan_anghelina@Yahoo.com>
2023-09-07 21:52:38 +0000

    It adds variables in the edit_template, but there are not directly displayed under "+ Variable" button.

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, redirect, url_for, flash, request, jsonify
 from flask_login import current_user, LoginManager, UserMixin, login_user, login_required, logout_user
+from sqlalchemy.exc import OperationalError, InvalidRequestError, PendingRollbackError, InterfaceError
 from sqlalchemy.orm.exc import NoResultFound
-from database import Template, session
+from database import Template, Session, session
 import re
 
 app = Flask(__name__)
@@ -127,68 +128,39 @@ def saved_templates():
     return render_template('saved_templates.html', templates=templates)
 
 
-@app.route('/edit_template/<int:template_id>', methods=['GET', 'POST'])
+
+
+
+
+@app.route('/edit_template/<int:template_id>', methods=['GET'])
 @login_required
 def edit_template(template_id):
-    template = session.query(Template).filter_by(id=template_id, user_id=current_user.id).first()
-
-    if not template:
-        flash('Template not found.')
+    template_data = session.query(Template).filter_by(id=template_id, user_id=current_user.id).first()
+    if template_data is None:
+        flash("Template not found.")
         return redirect(url_for('saved_templates'))
 
-    print("Template Variables:", template.template_variables)  # Debugging print statement
+    variables = template_data.template_variables.split(",") if template_data.template_variables else []
+    return render_template('edit_template.html', template=template_data, variables=variables,
+                           template_name=template_data.template_name, template_id=template_id,
+                           template_content=template_data.template_content)  # Added this line
 
-    # Extract variables using regular expression
-    if template.template_variables:
-        variables = re.findall(r'\[([^\]]+)\]', template.template_variables)
-    else:
-        variables = []
 
-    print("Extracted Variables:", variables)  # Add this line for debugging
 
-    if request.method == 'POST':
-        if 'update_template' in request.form:
-            template.template_name = request.form['template_name']
-            template.template_content = request.form['template_content']
-            session.commit()
-            flash('Template updated successfully!')
-            return redirect(url_for('edit_template', template_id=template_id))
 
-        elif 'add_variable' in request.form:
-            variable_name = request.form['variable_name']
-            formatted_variable = format_variable(variable_name)
 
-            if not formatted_variable:
-                flash('Variable can only contain letters, numbers, and underscores.')
-                return redirect(url_for('edit_template', template_id=template_id))
 
-            current_variables = template.template_variables.split(",") if template.template_variables else []
-            current_variables.append(formatted_variable)
-            template.template_variables = ",".join(current_variables)
-            session.commit()
-            flash(f'Variable {formatted_variable} added successfully!')
-            return redirect(url_for('edit_template', template_id=template_id))
 
-        elif 'delete_variable' in request.form:
-            variable_to_delete = request.form['delete_variable']
-            current_variables = template.template_variables.split(",") if template.template_variables else []
-            if variable_to_delete in current_variables:
-                current_variables.remove(variable_to_delete)
-                template.template_variables = ",".join(current_variables)
-                session.commit()
-                flash(f'Variable {variable_to_delete} deleted successfully!')
-            return redirect(url_for('edit_template', template_id=template_id))
 
-    variables = template.template_variables.split(",") if template.template_variables else []
-
-    return render_template('edit_template.html', template_name=template.template_name, template_id=template_id, variables=variables, template_content=template.template_content)
 
 
 @app.route('/get_template_data', methods=['GET'])
 def get_template_data():
     template_id = request.args.get('template_id')
     template = session.query(Template).filter_by(id=template_id).first()
-    
+
+    print("Debug:", template)  # For debugging
+
     if not template:
         return jsonify(success=False, message="Template not found"), 404
 
@@ -197,6 +169,7 @@ def get_template_data():
         template_content=template.template_content,
         template_variables=template.template_variables.split(",") if template.template_variables else []
     )
+
 
 
 @app.route('/logout')
@@ -239,34 +212,102 @@ def get_variables():
 
 
 
+
+
+
+
+
+
+
+
 @app.route('/add_variable', methods=['POST'])
 def add_variable():
     variable_name = request.form.get('variable_name')
+    template_id = request.form.get('template_id')
+    print(f"Debug: Received template_id: {template_id}")  # Debug line
     formatted_variable = format_variable(variable_name)
+    response = {}
 
     if not formatted_variable:
-        response = {
-            'status': 'error',
-            'message': 'Variable can only contain letters, numbers, and underscores.'
-        }
+        response['status'] = 'error'
+        response['message'] = 'Variable can only contain letters, numbers, and underscores.'
+        return jsonify(response), 400
     else:
-        response = {
-            'status': 'success',
-            'variable': formatted_variable
-        }
+        try:
+            if template_id:
+                # This is for edit_template
+                template = session.query(Template).filter_by(id=template_id, user_id=current_user.id).first()
+                if not template:
+                    response['status'] = 'error'
+                    response['message'] = 'Template not found.'
+                    return jsonify(response), 400
+            else:
+                # This is for new_template (draft)
+                template = session.query(Template).filter_by(draft=True, user_id=current_user.id).first()
+                if not template:
+                    template = Template(template_name="Draft", template_content='', draft=True, user_id=current_user.id)
+                    session.add(template)
+                    session.commit()
+            
+            current_variables = template.template_variables.split(",") if template.template_variables else []
+            if formatted_variable not in current_variables:
+                current_variables.append(formatted_variable)
+                template.template_variables = ",".join(current_variables)
+                session.commit()
 
-        draft = session.query(Template).filter_by(draft=True, user_id=current_user.id).first()
-        if not draft:
-            draft = Template(template_name="Draft", template_content='', draft=True, user_id=current_user.id)
-            session.add(draft)
-            session.commit()
 
-        current_variables = draft.template_variables.split(",") if draft.template_variables else []
-        current_variables.append(formatted_variable)
-        draft.template_variables = ",".join(current_variables)
+                response['status'] = 'success'
+                response['message'] = f'Variable {formatted_variable} added successfully!'
+                return jsonify(response), 200
+            else:
+                response['status'] = 'error'
+                response['message'] = f'Variable {formatted_variable} already exists.'
+                return jsonify(response), 400
+
+        except Exception as e:
+            response['status'] = 'error'
+            response['message'] = f'An unexpected error occurred: {str(e)}'
+            return jsonify(response), 500
+
+
+
+
+
+
+
+@app.route('/delete_variable', methods=['POST'])
+@login_required
+def delete_variable():
+    variable_to_delete = request.form.get('variable_name')
+    template_id = request.form.get('template_id')
+    print(f"Debug: Received template_id: {template_id}")  # Debug line
+
+    if not variable_to_delete:
+        return jsonify(status='error', message='Variable name is required.')
+
+    template = None
+    if template_id:
+        template = session.query(Template).filter_by(id=template_id, user_id=current_user.id).first()
+    else:
+        template = session.query(Template).filter_by(draft=True, user_id=current_user.id).first()
+    
+    if not template:
+        return jsonify(status='error', message='No appropriate template found.')
+
+    current_variables = template.template_variables.split(",") if template.template_variables else []
+    if variable_to_delete in current_variables:
+        current_variables.remove(variable_to_delete)
+        template.template_variables = ",".join(current_variables)
         session.commit()
+        return jsonify(status='success', message=f'Variable {variable_to_delete} deleted successfully!')
+    else:
+        return jsonify(status='error', message=f'Variable {variable_to_delete} not found.')
 
-    return jsonify(response)
+
+
+
+
+
 
 
 @app.route('/update_template', methods=['POST'])
@@ -292,34 +333,11 @@ def update_template():
             'message': 'Template updated successfully.'
         }
 
-    return jsonify(response)
+    return jsonify({'status': 'success', 'message': 'Template updated successfully.'})
 
 
-@app.route('/delete_variable', methods=['POST'])
-def delete_variable():
-    variable_to_delete = request.form.get('variable_name')
 
-    if not variable_to_delete:
-        return jsonify(status='error', message='Variable name is required.')
 
-    try:  # Start of try block
-        draft = session.query(Template).filter_by(draft=True, user_id=current_user.id).first()
-        if not draft:
-            return jsonify(status='error', message='No draft template found.')
-
-        current_variables = draft.template_variables.split(",") if draft.template_variables else []
-        if variable_to_delete in current_variables:
-            current_variables.remove(variable_to_delete)
-            draft.template_variables = ",".join(current_variables)
-            session.commit()
-            return jsonify(status='success', message=f'Variable {variable_to_delete} deleted successfully!')
-        else:
-            return jsonify(status='error', message=f'Variable {variable_to_delete} not found.')
-
-    except Exception as e:  # Handle any exception here
-        # Log the error for debugging purposes
-        print(f"Database error: {e}")
-        return jsonify(status='error', message='An internal error occurred. Please try again later.')
 
 
 @app.route('/extract_variables', methods=['GET', 'POST'])
